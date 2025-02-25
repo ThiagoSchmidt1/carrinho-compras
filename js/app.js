fdd32156ccef405a3b7ca2c7a11b1ec4f4d4948c
ThiagoSchmidt1 <168141973+ThiagoSchmidt1@users.noreply.github.com>
2025-02-25 15:29:03 -0300

    Quantidade, valor e preço

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 function adicionar() {
     //recuperar valores nome do produto, quantidade e valor 
-    let nomeProduto = document.getElementById('produto'); 
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto.value);
+    alert(nomeProduto);
+    alert(valorUnitario);
     alert(quantidade.value);
 //calcular o preço e nosso subtotal
+    let preco = quantidade.value * valorUnitario;
+    alert(preco);
 //adicionar no carrinho
 //atualizar o valor total
 
