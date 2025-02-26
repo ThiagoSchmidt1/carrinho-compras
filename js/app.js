b2f9e36b5e222b055ab882272585a584e1d9802c
ThiagoSchmidt1 <168141973+ThiagoSchmidt1@users.noreply.github.com>
2025-02-26 12:23:39 -0300

    valor total

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
     //recuperar valores nome do produto, quantidade e valor 
     let produto = document.getElementById('produto').value;
@@ -12,9 +15,15 @@ function adicionar() {
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
   </section>`;
 //atualizar o valor total
+totalGeral = totalGeral + preco;
 
+let campoTotal = document.getElementById('valor-total');
+campoTotal.textContent = `R$ ${totalGeral}`;
+document.getElementById('quantidade').value = 0
 }
 
 function limpar() {
-    
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';   
 }
