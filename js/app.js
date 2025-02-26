34249d400fe05e438a5ce9933c3ad1b23a38df7e
ThiagoSchmidt1 <168141973+ThiagoSchmidt1@users.noreply.github.com>
2025-02-26 11:18:49 -0300

    adicionar carrinho

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
+    let quantidade = document.getElementById('quantidade').value;
 //calcular o preço e nosso subtotal
-    let preco = quantidade.value * valorUnitario;
-    alert(preco);
+    let preco = quantidade * valorUnitario;
 //adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
 //atualizar o valor total
 
 }
